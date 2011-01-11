3ac92a52119586274cb2b452332bf5c3ef92cc7e
Leo Lapworth <leo@cuckoo.org>
2011-01-11 21:57:05 +0000

    Bump to 2.10 for release

--- lib/Text/vCard.pm
+++ lib/Text/vCard.pm
@@ -10,7 +10,7 @@ use Text::vCard::Node;
 # See this module for your basic parser functions
 use base qw(Text::vFile::asData);
 use vars qw ($VERSION %lookup %node_aliases @simple);
-$VERSION = '2.09';
+$VERSION = '2.10';
 
 # If the node's data does not break down use this
 my @default_field = qw(value);
