cb3da8055b73093522a1868a79f9543fe091c965
Eric Johnson <eric.git@iijo.org>
2013-12-27 17:26:33 +0000

    Filled out vCard::AddressBook + tests

--- lib/vCard/AddressBook.pm
+++ lib/vCard/AddressBook.pm
@@ -1,8 +1,10 @@
 package vCard::AddressBook;
-
+use feature qw/say/;
 use Moo;
-use Text::vCard;
+use vCard;
 use Path::Class;
+use Carp;
+use Text::vCard;
 
 =head1 SYNOPSIS
 
@@ -16,10 +18,10 @@ use Path::Class;
 
     my $vcard = $adress_book->add_vcard; # returns a vCard object
     $vcard->fullname('Bruce Banner, PhD');
-    $vcard->email_addresses({
+    $vcard->email_addresses([
         { type => 'work', address => 'bbanner@ssh.secret.army.mil' },
         { type => 'home', address => 'bbanner@timewarner.com'      },
-    });
+    ]);
 
     # $address_book->vcards() returns a L<vCard> object
     foreach my $vcard ( $address_book->vcards() ) {
@@ -42,5 +44,121 @@ interface.
 =head1 METHODS
 
 =cut
+
+has vcards => ( is => 'rw', default => sub { [] } );
+
+sub add_vcard {
+    my ($self) = @_;
+    my $vcard = vCard->new;
+    push @{ $self->vcards }, $vcard;
+    return $vcard;
+}
+
+sub load_file {
+    my ( $self, $filename ) = @_;
+
+    my $cards = $self->_parse_file($filename);
+
+    foreach my $card (@$cards) {
+        carp "This file contains $card->{'type'} data which was not parsed"
+            unless $card->{'type'} =~ /VCARD/i;
+
+        my $vcard      = vCard->new;
+        my $text_vcard = Text::vCard    #
+            ->new( { 'asData_node' => $card->{'properties'} } );
+
+        $self->_copy_simple_nodes( $text_vcard => $vcard );
+        $self->_copy_phones( $text_vcard => $vcard );
+        $self->_copy_addresses( $text_vcard => $vcard );
+        $self->_copy_email_addresses( $text_vcard => $vcard );
+
+        push @{ $self->vcards }, $vcard;
+    }
+
+    return $self;
+}
+
+sub _parse_file {
+    my ( $self, $filename ) = @_;
+
+    my $file = ref $filename eq 'Path::Class::File'    #
+        ? $filename
+        : file($filename);
+
+    my $asData = Text::vFile::asData->new;
+    $asData->preserve_params(1);
+
+    return $asData->parse_lines( $file->slurp )->{'objects'};
+}
+
+sub _copy_simple_nodes {
+    my ( $self, $text_vcard, $vcard ) = @_;
+
+    foreach my $node_type ( vCard->_simple_node_types ) {
+        next unless $text_vcard->$node_type;
+        say $node_type;
+        $vcard->$node_type( $text_vcard->$node_type );
+    }
+}
+
+sub _copy_phones {
+    my ( $self, $text_vcard, $vcard ) = @_;
+
+    my @phones;
+    my $nodes = $text_vcard->get('tel') || [];
+
+    foreach my $node (@$nodes) {
+        my $phone;
+        $phone->{type}       = $node->types()->[0];      # FIXME
+        $phone->{preference} = $node->is_pref ? 1 : 0;
+        $phone->{number}     = $node->value;
+        push @phones, $phone;
+    }
+
+    $vcard->phones( \@phones );
+}
+
+sub _copy_addresses {
+    my ( $self, $text_vcard, $vcard ) = @_;
+
+    my @addresses;
+    my $nodes = $text_vcard->get('adr') || [];
+
+    foreach my $node (@$nodes) {
+        my $address;
+        $address->{type}       = $node->types()->[0];         # FIXME
+        $address->{preference} = $node->is_pref ? 1 : 0;
+        $address->{po_box}     = $node->po_box || undef;
+        $address->{street}     = $node->street || undef;
+        $address->{city}       = $node->city || undef;
+        $address->{post_code}  = $node->post_code || undef;
+        $address->{region}     = $node->region || undef;
+        $address->{country}    = $node->country || undef;
+        $address->{extended}   = $node->extended || undef;
+        push @addresses, $address;
+    }
+
+    $vcard->addresses( \@addresses );
+}
+
+sub _copy_email_addresses {
+    my ( $self, $text_vcard, $vcard ) = @_;
+
+    my @email_addresses;
+    my $nodes = $text_vcard->get('email') || [];
+
+    foreach my $node (@$nodes) {
+        say "hiiiiiiiiiiiiiiiiiiiiiiiiii";
+        use DDP;
+        p $node;
+        my $email_address;
+        $email_address->{type}       = $node->types()->[0];      # FIXME
+        $email_address->{preference} = $node->is_pref ? 1 : 0;
+        $email_address->{address}    = $node->value;
+        push @email_addresses, $email_address;
+    }
+
+    $vcard->email_addresses( \@email_addresses );
+}
 
 1;
---
+++ t/vcard/address_book.t
@@ -0,0 +1,74 @@
+use Test::Most;
+
+use Encode;
+use Directory::Scratch;
+use Path::Class;
+use vCard::AddressBook;
+
+my $in_file = file('t/simple.vcf');
+
+#my $tmp_file     = Directory::Scratch->new->touch('.simple.vcf');
+my $address_book = vCard::AddressBook->new;
+$address_book->add_vcard;
+$address_book->add_vcard;
+$address_book->add_vcard;
+
+# TODO: get rid of the undef tests by loading a vcf which tests every field
+subtest 'load_file()' => sub {
+    $address_book->load_file($in_file);
+    my $vcard = $address_book->vcards->[3];
+
+    note "simple getters and setters";
+    is $vcard->fullname, 'T-firstname T-surname', 'fullname()';
+    is $vcard->title,    undef,                   'title()';
+    is $vcard->photo,    undef,                   'photo()';
+    is $vcard->birthday, undef,                   'birthday()';
+    is $vcard->timezone, undef,                   'timezone()';
+
+    note "complex getters and setters";
+    is_deeply $vcard->phones,
+        [
+        { type => 'home', number => '020 666 6666',  preference => 0 },
+        { type => 'cell', number => '0793 777 7777', preference => 0 }
+        ],
+        'phones()';
+    is_deeply $vcard->addresses,
+        [
+        {   type       => 'home',
+            preference => 0,
+            po_box     => undef,
+            street     => 'Test Road',
+            city       => 'Test City',
+            region     => undef,
+            post_code  => 'Test Postcode',
+            country    => 'Test Country',
+            extended   => undef,
+        },
+        {   type       => 'home',
+            preference => 1,
+            po_box     => undef,
+            street     => 'Pref Test Road',
+            city       => 'Pref Test City',
+            region     => undef,
+            post_code  => 'Pref Test Postcode',
+            country    => 'Pref Test Country',
+            extended   => undef,
+        }
+        ],
+        'addresses()';
+    is_deeply $vcard->email_addresses, [], 'email_addresses()';
+    ##is_deeply $vcard->email_addresses,
+    ##    [
+    ##    {   type       => 'work',
+    ##        address    => 'jqpublic@xyz.example.com',
+    ##        preference => 0
+    ##    }
+    ##    ],
+    ##    'email_addresses()';
+
+};
+
+is scalar @{ $address_book->vcards }, 4, 'created the right number of vcards';
+is ref $_, 'vCard', 'object reference' for @{ $address_book->vcards };
+
+done_testing;
